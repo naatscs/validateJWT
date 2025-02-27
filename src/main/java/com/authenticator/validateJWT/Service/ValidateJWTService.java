3c3cccc17da5bace7f95e67c3f45edf019f2baf4
naatscs <naatscs@gmail.com>
2025-02-27 15:04:50 -0300

    addint jwtContainNameRoleAndSeedClaims method

--- src/main/java/com/authenticator/validateJWT/Service/ValidateJWTService.java
+++ src/main/java/com/authenticator/validateJWT/Service/ValidateJWTService.java
@@ -4,19 +4,28 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
+import java.util.Set;
+
 public class ValidateJWTService {
     public boolean validateTokenService(String token){
         try {
             DecodedJWT decodedJWT = JWT.decode(token);
+
+            return jwtContainNameRoleAndSeedClaims(decodedJWT);
+
         }catch (JWTDecodeException e){
             System.err.println("Invalid JWT token: " + e.getMessage());
             return false;
         }
-        return true;
     }
 
-    public boolean jwtContainClaims(String token){
-        return false;
+    public boolean jwtContainNameRoleAndSeedClaims(DecodedJWT decodedJWT){
+        Set<String> claimNames = decodedJWT.getClaims().keySet();
+        Set<String> expectedClaims = Set.of("Name", "Role", "Seed");
+
+        return claimNames.equals(expectedClaims);
+
+
     }
 
     public boolean clainNameContainsNumbers(String token){
